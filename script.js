63d3bfc271d9d670bfcd5d1443f40a5e9ff296ef
sunny2831 <sunny2831@hotmail.co.uk>
2020-12-05 21:21:53 +0000

    added start game again functionality

--- script.js
+++ script.js
@@ -10,9 +10,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let gameScore = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -26,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // when the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     // when the guess is too low
@@ -49,4 +48,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  gameScore = 20;
+  document.querySelector('.score').textContent = '20';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
 });
